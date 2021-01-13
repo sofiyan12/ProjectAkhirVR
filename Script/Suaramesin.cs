9c1006559192f3c523d41d5f6f071273d58fcc0b
sofiyan12 <59703572+sofiyan12@users.noreply.github.com>
2021-01-14 01:12:56 +0700

    Add files via upload

---
+++ Script/Suaramesin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Suaramesin : MonoBehaviour, ITrackableEventHandler
+{
+    GameObject BSound, BVideo;
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BSound = GameObject.Find("Suaramesin");
+        BVideo = GameObject.Find("buttonmesin");
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        BSound.SetActive(true);
+        BVideo.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "buttongray")
+                {
+                    BSound.SetActive(false);
+                    BVideo.SetActive(true);
+                    BSound.GetComponent<AudioSource>().Play();
+                }
+
+                if (hit.collider.tag == "buttonmesin")
+                {
+                    BSound.SetActive(true);
+                    BVideo.SetActive(false);
+                    BVideo.GetComponent<AudioSource>().Stop();
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Script/buttongray.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class buttongray : MonoBehaviour, ITrackableEventHandler
+{
+    GameObject BSound, BVideo;
+
+    private TrackableBehaviour mTrackableBehaviour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BSound = GameObject.Find("buttongray");
+        BVideo = GameObject.Find("buttonmesin");
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        BSound.SetActive(true);
+        BVideo.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "buttongray")
+                {
+                    BSound.SetActive(false);
+                    BVideo.SetActive(true);
+                    BSound.GetComponent<AudioSource>().Play();
+                }
+
+                if (hit.collider.tag == "buttonmesin")
+                {
+                    BSound.SetActive(true);
+                    BVideo.SetActive(false);
+                    BVideo.GetComponent<AudioSource>().Stop();
+                }
+            }
+        }
+    }
+}
